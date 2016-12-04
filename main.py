d5711660cb049ab83fa7d0d3199b3262e88ea43e
Josiah <spacetagman@gmail.com>
2016-12-03 19:33:02 -0600

    Working subscription

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """Main python file for chat client"""
 # This is meant to run in Python 3, not Python 2
+# DISCLAIMER: Some code borrowed from hbmqtt library example documentation:
+# https://hbmqtt.readthedocs.io/en/latest/references/mqttclient.html
 
+import asyncio
 import hbmqtt
+from hbmqtt import client as hbmqttclient
 from threading import Thread
 import queue
 from time import sleep
 
+MQTT_BROKER_URI = "mqtt://localhost:1883"
 inputQueue = queue.Queue()
 
 # - We are using threads so we can get user input while listening for messages
@@ -30,17 +35,41 @@ def getNextInputLine():
         inputLine = input()
         inputQueue.put(inputLine)
 
-inputThread = Thread(target=getNextInputLine)
-inputThread.start()
+# inputThread = Thread(target=getNextInputLine)
+# inputThread.start()
 
-for i in range(100):
-    # Try to get user input
-    try:
-        inputLine = inputQueue.get_nowait()
-        print("Oh hey! A message from the user:")
-        print(inputLine)
-    except queue.Empty:
-        pass # No new user input
-    print(i)
-    sleep(4)
+
+
+@asyncio.coroutine
+def beginMQTTClient():
+    client = hbmqttclient.MQTTClient()
+    yield from client.connect(MQTT_BROKER_URI)
+    yield from client.subscribe([("blue", hbmqtt.mqtt.constants.QOS_0)])
+    while True:
+        msg = yield from client.deliver_message()
+        # Help from internal python docs on bytearray
+        rawStrMsg = msg.data.decode("utf-8")
+        clientId = rawStrMsg[0]
+        textMessage = rawStrMsg[1:]
+
+        print("Message received from client " + clientId + ": " + textMessage)
+
+
+asyncio.get_event_loop().run_until_complete(beginMQTTClient())
+
+# while True:
+    # x = client.deliver_message()
+    # print("Message received!")
+    # print x
+
+# for i in range(100):
+#     # Try to get user input
+#     try:
+#         inputLine = inputQueue.get_nowait()
+#         print("Oh hey! A message from the user:")
+#         print(inputLine)
+#     except queue.Empty:
+#         pass # No new user input
+#     print(i)
+#     sleep(.2)
 
